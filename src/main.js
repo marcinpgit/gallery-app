05bd4bbde79db2887edd9d5c8d21353b02bc99da
marcinpgit <marcinp2012@gmail.com>
2018-06-13 15:03:27 +0200

    adding bootstrap-vue and v-img

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import VueImg from 'v-img'
+
+Vue.use(VueImg)
+Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
 
