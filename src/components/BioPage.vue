7f41a9e1d1da612a39ec3113fd6ab66e4559da80
marcinpgit <marcinp2012@gmail.com>
2018-06-14 12:38:04 +0200

    descritption extended, assests added to BioPage

--- src/components/BioPage.vue
+++ src/components/BioPage.vue
@@ -17,12 +17,21 @@
                 </div>
                 <div class="bio-client">
                     <h4>Client List</h4>
-                    
+                    <ul class="client-list" v-for="client in clients">
+                        <li>{{ client.name }}</li>
+                    </ul>
                 </div>
                 <div class="bio-books">
                     <h4>Books</h4>
-                
+                    <ul class="books-list" v-for="book in books">
+                    <li>{{ book.title }}</li>
+                    </ul>
                 </div>
+            </b-col>
+            
+            <b-col sm="12" md="6" class="bio-picture">
+                <b-img class="artist-image" fluid :src="authorImage" :alt="altAuthor"/>
+                <b-img class="view-image" fluid :src="viewImage" :alt="altView"/>
             </b-col>
 
         </b-row> 
@@ -33,6 +42,33 @@
 
 <script>
 export default {
-    name: 'bioPage'
+    name: 'bioPage',
+    data (){
+        return {
+            altAuthor: 'Author picture',
+            altView: 'View image',
+            authorImage: require('../assets/images/bio/bio-artist.jpg'),
+            viewImage: require('../assets/images/bio/bio-picture.jpg'),
+            clients: [
+                { name: 'Bueno Restaurant' },
+                { name: 'Delicious Appetito' },
+                { name: 'Healthy Carrot' },
+                { name: 'Tommy Smith' },
+                { name: 'Pea Restaurant' },
+                { name: 'Food Market' },
+                { name: 'Marshall Brothers' },
+                { name: 'Diet Catering' },
+                { name: 'Alexa Donovan Restaurant' },
+            ],
+            books: [
+                { title: 'The Healthy Food' },
+                { title: 'Healing of Cucumber' },
+                { title: 'The Path of Vegetables' },
+                { title: 'Enchanted Polish Cuisine' },
+                { title: 'Food for Kids' },
+                { title: 'Healthy Diet' }
+            ]
+        }
+    }
 }
 </script>
