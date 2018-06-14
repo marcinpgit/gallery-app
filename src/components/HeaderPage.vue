a0952c65c25244fc4b32d0b9b8e97f3cb1481795
marcinpgit <marcinp2012@gmail.com>
2018-06-14 12:00:45 +0200

    styles added to HeaderPage

--- src/components/HeaderPage.vue
+++ src/components/HeaderPage.vue
@@ -32,3 +32,14 @@ export default {
     name: 'headerPage',
 }
 </script>
+
+<style scoped>
+    .navbar-expand-md .navbar-nav .nav-link { 
+        color: #fff;
+        padding-left: 40px;
+        padding-right: 40px
+    }
+    .bg-info {
+        background-color: #2C3E50 !important
+    }
+</style>
