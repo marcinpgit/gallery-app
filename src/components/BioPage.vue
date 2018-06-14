9ce5901c2beb2e425ed77fd05937294c4262d751
marcinpgit <marcinp2012@gmail.com>
2018-06-14 12:30:53 +0200

    BioPage description added

--- src/components/BioPage.vue
+++ src/components/BioPage.vue
@@ -6,6 +6,14 @@
             <b-col sm="12" md="6">
                 <div class="bio-description">
                     <h4>Bio</h4>
+                    <h5 class="hello-text">Hi, I'm Alexandra!</h5>
+                    Clad sometimes in muddy boots, sometimes in a pair of heels, 
+                    my career as a photographer often requires both sets 
+                    of footwear as I follow my clients on their adventures 
+                    all over the world. Catamarans, towering ballrooms, glacial lakes, 
+                    rooftops, mountains, bed and breakfasts, mossy forests, craggy 
+                    coastlines—my spirit is undaunted, and I am forever inspired by people, 
+                    nature, wind and light.
                 </div>
                 <div class="bio-client">
                     <h4>Client List</h4>
